be540ee605b0fde45dd715dd0aed703341f2bc18
SweetGamma <114316364+SweetGamma@users.noreply.github.com>
2024-12-02 10:04:03 -0700

    exported from Skill Struck

---
+++ Adding to Lists in Python/Last Minute Treats.py
@@ -0,0 +1,9 @@
+first = input("What other item do you want?")
+second = input("What final item do you want?")
+
+treats = ["popcorn", "popsicles", "soda", "chips", "cookies"]
+
+treats.append(first)
+treats.append(second)
+
+print(treats)
