8d12cc4b8d858d63e123b9a1c70d609e3c889cee
SweetGamma <114316364+SweetGamma@users.noreply.github.com>
2024-12-02 10:04:04 -0700

    exported from Skill Struck

---
+++ Adding to Lists in Python/Olympic Events.py
@@ -0,0 +1,8 @@
+olympics = ["running", "gymnastics", "swimming", "volleyball", "basketball"]
+new_olympics = ["karate", "surfing", "baseball", "skateboarding", "sport climbing"]
+
+
+olympics.extend(new_olympics)
+
+
+print(olympics)
