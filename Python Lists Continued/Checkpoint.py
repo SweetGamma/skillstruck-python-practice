22ea49f4e2d2bb68599e885f8345badcd9968820
SweetGamma <114316364+SweetGamma@users.noreply.github.com>
2024-12-02 10:04:07 -0700

    exported from Skill Struck

---
+++ Python Lists Continued/Checkpoint.py
@@ -0,0 +1,13 @@
+activities = ["movies", "skating", "bowling", "laser tag", "escape room", "trampoline park", "library"]
+
+# 1. Print the first 3 values from the list using slicing
+print(activities[:3])
+
+# 2. Replace the last item in the list with "water park"
+activities[-1] = "water park"
+
+# 3. Print the updated list after replacing the last item
+print(activities)
+
+# 4. Print the length of the list
+print(len(activities))
