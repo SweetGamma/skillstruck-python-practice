a76170042cb3257aa971b72ae80ddb3fc39400f6
SweetGamma <114316364+SweetGamma@users.noreply.github.com>
2024-12-02 10:04:31 -0700

    exported from Skill Struck

---
+++ File Write Mode/Checkpoint.py
@@ -0,0 +1,10 @@
+file = open("example.txt", "w")
+file.write("In short, I love to code!")  
+
+file.close()  
+
+
+file = open("porcupine.txt", "w")
+file.write("This is the new porcupine file.")  
+
+file.close()  
