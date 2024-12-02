729ff78516f72a79ac377d58ac6e984a1f832c76
SweetGamma <114316364+SweetGamma@users.noreply.github.com>
2024-12-02 10:04:17 -0700

    exported from Skill Struck

---
+++ Python Dictionaries/Dictionaries and Lists.py
@@ -0,0 +1,16 @@
+packing = {
+    "shoes": 2,
+    "socks": 5,
+    "shirts": 3,
+    "pants": 4,
+    "pjs": 1
+}
+
+
+print(packing)
+
+
+packing_list = ["shoes", "socks", "shirts", "pants", "pjs"]
+
+
+print(packing_list)
