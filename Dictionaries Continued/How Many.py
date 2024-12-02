2c59c5f70a24092d3ea9ce5c90cd3150ab19b300
SweetGamma <114316364+SweetGamma@users.noreply.github.com>
2024-12-02 10:04:20 -0700

    exported from Skill Struck

---
+++ Dictionaries Continued/How Many.py
@@ -0,0 +1,11 @@
+dictionary = {
+    1: "bicycle",
+    2: "soccer balls",
+    3: "piano books"
+}
+
+dictionary[4] = input("What do you have 4 of? ")
+dictionary[5] = input("What do you have 5 of? ")
+dictionary[6] = input("What do you have 6 of? ")
+
+print(dictionary)
