69b42a23b85f16387670c721dc470b906dded591
SweetGamma <114316364+SweetGamma@users.noreply.github.com>
2024-12-02 10:04:16 -0700

    exported from Skill Struck

---
+++ Python Dictionaries/Dictionary Author.py
@@ -0,0 +1,16 @@
+# Create a dictionary with at least 10 key/value pairs
+holidays = {
+    "January": "New Year's Day",
+    "February": "Valentine's Day",
+    "March": "St. Patrick's Day",
+    "April": "April Fools' Day",
+    "May": "Labor Day",
+    "June": "Father's Day",
+    "July": "Independence Day",
+    "August": "Friendship Day",
+    "September": "Labor Day",
+    "October": "Halloween"
+}
+
+# Print the dictionary
+print(holidays)
