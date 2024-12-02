813027433958f6d3698a30edacc639c8099489fb
SweetGamma <114316364+SweetGamma@users.noreply.github.com>
2024-12-02 10:04:02 -0700

    exported from Skill Struck

---
+++ Using Intergers from a List/Checkpoint.py
@@ -0,0 +1,14 @@
+lasers = [3, 10, 4, 15, 11]
+
+
+print(lasers[2] * 10)
+
+# 2. Print the value of pi rounded to 3 decimal places.
+print(round(3.14159265, 3))
+
+for x in range(0,10):
+    print(x)
+
+for x in range(0,10,2):
+    print(x)
+
