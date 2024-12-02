bf6915028055a037dfa080a7ac529ccf59bd31ab
SweetGamma <114316364+SweetGamma@users.noreply.github.com>
2024-12-02 10:04:16 -0700

    exported from Skill Struck

---
+++ Python Dictionaries/Checkpoint.py
@@ -0,0 +1,16 @@
+# Create the dictionary named mountains
+mountains = {
+    "Timpanogos": 4,
+    "Everest": 10,
+    "Kilimanjaro": 9, 
+    "Vesuvius": 6
+}
+print(mountains)
+
+print(mountains["Vesuvius"])
+
+
+mountains["Kilimanjaro"] = 12
+
+
+print(mountains)
