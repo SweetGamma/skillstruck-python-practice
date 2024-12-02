cd2b21b54772faf4e6c455a01456b44db35430d3
SweetGamma <114316364+SweetGamma@users.noreply.github.com>
2024-12-02 10:04:05 -0700

    exported from Skill Struck

---
+++ Removing from Lists in Python/Skills.py
@@ -0,0 +1,9 @@
+choice = input("What skill do you want to remove?")
+
+skills = ["Piano", "Soccer", "Coding", "Cooking", "Writing"]
+
+# Remove the inputted skill from the list
+skills.remove(choice)
+
+# Print the updated list
+print(skills)
