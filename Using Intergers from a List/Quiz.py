90b9eb8eed9c1dd7346ae68eac9d192a59e30dbb
SweetGamma <114316364+SweetGamma@users.noreply.github.com>
2024-12-02 10:04:00 -0700

    exported from Skill Struck

---
+++ Using Intergers from a List/Quiz.py
@@ -0,0 +1,34 @@
+#Question number 1
+#goldfish = [4, 8, 1, 3, 10]
+#print(goldfish[2] + 1)
+
+#Question number 2
+#dessert = 12.4635
+#print(round(dessert, 2))
+
+#Question number 3
+#for x in range(3):
+	#print(x)
+
+#Question number 4
+#for x in range(0, 6, 2):
+	#print(x)
+
+#Question number 5
+#smells = ["skunk", "lilac", "rain", "ocean", "garbage", "cleaner", "cookies", "roses", "new car", "sweaty feet", "peach"]
+
+#print(smells[0:8:2])
+
+#Question number 7
+#jewels = [4, 10, 5, 2, 7]
+
+#print(jewels[3] + 10)
+
+#Question number 8
+#pizza = 5.879
+
+#print(round(pizza, 2))
+
+#Question number 9
+for x in range(10, 21, 10):
+    print(x)
